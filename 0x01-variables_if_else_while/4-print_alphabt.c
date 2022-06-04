9b6b8e044e8173cf9940137808127e9182dee0a6
Tesfamariam Tafesse <tafessemekete@gmail.com>
2022-06-04 02:21:57 -0700

    alphabet challeng

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main- This is main function
+ * Return: 0 if it runs with out error
+ */
+int main(void)
+{
+	char ch;
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch == 'e' || ch == 'q')
+		       continue;
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
