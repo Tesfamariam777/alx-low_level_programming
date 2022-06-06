d7ab82fa9cceebffc65dbec141f3ec57445b4648
Tesfamariam Tafesse <60731032+Tesfamariam777@users.noreply.github.com>
2022-06-06 14:12:49 +0300

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,15 +9,12 @@ int main(void)
 
 	for (num = 0; num <= 9; num++)
 	{
-        	putchar('0' + num);
+		putchar('0' + num);
 		if (num == 9)
 			continue;
-        	putchar(',');
-        	putchar(' ');
+		putchar(',');
+		putchar(' ');
 	}
 	putchar('\n');
 	return (0);
 }
-
-
-
