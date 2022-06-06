89dd41888021a67f6bfa0de5fbe21730c6522a01
Tesfamariam Tafesse <tafessemekete@gmail.com>
2022-06-06 05:34:57 -0700

    number challenge

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main- This is main function
+ * Return: 0 runs correct
+ */
+int main(void)
+{
+	short num1;
+	short num2;
+        
+	for (num1 = 0; num1 < 9; num1++)
+	{
+		for (num2 = 1; num2 < 10; num2++)
+		{
+			if (num1 == num2)
+				continue;
+			putchar('0' + num1);
+			putchar('0' + num2);
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
